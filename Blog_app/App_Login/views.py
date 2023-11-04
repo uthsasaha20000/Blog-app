91843c16cfd23b9f473b4ff940e5e2929b179ecc
uthsasaha <uthsasaha47@gmail.com>
2023-11-04 23:58:41 +0600

    minor added

--- Blog_app/App_Login/views.py
+++ Blog_app/App_Login/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import UserCreationForm
 
 # Create your views here.
+def sign_up(request):
+    form=UserCreationForm()
+    if request.method=='POST':
+        form

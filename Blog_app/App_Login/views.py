2311351f5d236546bd6cbf4ccde9692c2b082199
uthsasaha <uthsasaha47@gmail.com>
2023-11-05 23:19:53 +0600

    base html changed

--- Blog_app/App_Login/views.py
+++ Blog_app/App_Login/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.contrib.auth import UserCreationForm
+#from django.contrib.auth import UserCreationForm
 
 # Create your views here.
 def sign_up(request):
     form=UserCreationForm()
+    registered=False
     if request.method=='POST':
-        form
+        form=UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            registerd=True
+    dict={'form':form,'registered':registerd}
+    return render(request,'App_Login/sign_up.html',context=dict)

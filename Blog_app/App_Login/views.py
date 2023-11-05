3ed51c3293f1b8abe99207c0cd127e934043c5a9
uthsasaha <uthsasaha47@gmail.com>
2023-11-05 23:42:16 +0600

    edited signup html and its view

--- Blog_app/App_Login/views.py
+++ Blog_app/App_Login/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-#from django.contrib.auth import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def sign_up(request):
-    form=UserCreationForm()
-    registered=False
-    if request.method=='POST':
-        form=UserCreationForm(data=request.POST)
+    registered = False
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            registerd=True
-    dict={'form':form,'registered':registerd}
-    return render(request,'App_Login/sign_up.html',context=dict)
+            registered = True
+
+    context = {'form': form, 'registered': registered}
+    return render(request, 'App_Login/sign_up.html', context)

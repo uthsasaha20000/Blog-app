382ee0c091d0bb04d8b240040463878c17bb80a5
uthsasaha <uthsasaha47@gmail.com>
2023-11-15 11:02:09 +0600

    forms.py added

---
+++ Blog_app/App_Login/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from App_Login.models import UserProfile
+from string import Template
+from django.utils.safestring import mark_safe
+from django.forms import ImageField
+
+class CreateNewUser(UserCreationForm):
+    email = forms.EmailField(required=True, label="", widget=forms.TextInput(attrs={'placeholder':'Email'}))
+    username = forms.CharField(required=True, label="", widget=forms.TextInput(attrs={'placeholder':'Username'}))
+    password1 = forms.CharField(
+        required = True,
+        label = "",
+        widget = forms.PasswordInput(attrs={'placeholder':'Password'})
+    )
+    password2 = forms.CharField(
+        required = True,
+        label = "",
+        widget = forms.PasswordInput(attrs={'placeholder':'Password Confirmation'})
+    )
+
+
+    class Meta:
+        model = User
+        fields = ('email','username','password1','password2')

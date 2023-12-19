f47d75a815551917f559b4ac09c3d271c7631a97
uthsasaha <uthsasaha47@gmail.com>
2023-12-19 16:51:26 +0600

    simple change

--- Blog_app/App_Login/models.py
+++ Blog_app/App_Login/models.py
@@ -9,6 +9,7 @@ class UserProfile(models.Model):
     user=models.OneToOneField(User,related_name='user_profile',on_delete=models.CASCADE)
     profile_pic=models.ImageField(upload_to='profile_pics')
     dob=models.DateField(null=True)
+    description = models.TextField(blank=True)
     city=models.CharField(null=True,max_length=15)
     bio=models.CharField(null=True,max_length=50)
 

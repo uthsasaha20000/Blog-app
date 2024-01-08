e345f5e1241c66d7b703c2af68ba2950fca1a3d2
uthsasaha <uthsasaha47@gmail.com>
2024-01-09 01:02:09 +0600

    search function

--- Blog_app/App_Blog/views.py
+++ Blog_app/App_Blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
-
+from django.contrib.auth.models import User
+from App_Login.models import UserProfile
 # Create your views here.
 @login_required
 def blog_list(request):
-    return render(request,'App_Blog/blog_list.html',context={})
+  if request.method=='GET':
+   search=request.GET.get('search','')
+   result=User.objects.filter(username__icontains=search)
+
+   return render(request,'App_Blog/blog_list.html',context={'search':search,'result':result})

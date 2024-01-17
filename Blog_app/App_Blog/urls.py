551b99bc884352d7e176e37d6676c919ea5b7823
uthsasaha <uthsasaha47@gmail.com>
2024-01-18 01:52:23 +0600

    partial completed

--- Blog_app/App_Blog/urls.py
+++ Blog_app/App_Blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 app_name='App_Blog'
 urlpatterns=[
-    path('',views.blog_list,name='home')
+    path('',views.blog_list,name='home'),
+    path('liked/<pk>/', views.liked, name='liked'),
+    path('unliked/<pk>/',views.unliked, name='unliked'),
 ]
--- Blog_app/App_Blog/views.py
+++ Blog_app/App_Blog/views.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
+
+from App_Login.models import UserProfile, Follow
 from django.contrib.auth.models import User
-from App_Login.models import UserProfile
+
+from App_Blog.models import Post, Like
+
 # Create your views here.
 @login_required
 def blog_list(request):
-  if request.method=='GET':
-   search=request.GET.get('search','')
-   result=User.objects.filter(username__icontains=search)
+    following_list = Follow.objects.filter(follower=request.user)
+    posts = Post.objects.filter(author__in=following_list.values_list('following'))
+    liked_post = Like.objects.filter(user=request.user)
+    liked_post_list = liked_post.values_list('post', flat=True)
+    print(liked_post_list)
+    if request.method == 'GET':
+        search = request.GET.get('search', '')
+        result = User.objects.filter(username__icontains=search)
+    return render(request, 'App_Blog/blog_list.html', context={'title':'Home', 'search':search, 'result':result, 'posts':posts, 'liked_post_list':liked_post_list})
 
-   return render(request,'App_Blog/blog_list.html',context={'search':search,'result':result})
+
+@login_required
+def liked(request, pk):
+    post = Post.objects.get(pk=pk)
+    already_liked = Like.objects.filter(post=post, user=request.user)
+    if not already_liked:
+        liked_post = Like(post=post, user=request.user)
+        liked_post.save()
+    return HttpResponseRedirect(reverse('home'))
+
+@login_required
+def unliked(request, pk):
+    post = Post.objects.get(pk=pk)
+    already_liked = Like.objects.filter(post=post, user=request.user)
+    already_liked.delete()
+    return HttpResponseRedirect(reverse('home'))
